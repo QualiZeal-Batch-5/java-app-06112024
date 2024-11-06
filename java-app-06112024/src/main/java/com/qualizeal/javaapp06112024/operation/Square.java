fab12aba80cccba3a9e50132b8cb738349d3f438
Likhitha Boorsu <twinkleboorsu1717@gmail.com>
2024-11-06 11:23:15 +0530

    addingfiles

--- java-app-06112024/src/main/java/com/qualizeal/javaapp06112024/operation/Square.java
+++ java-app-06112024/src/main/java/com/qualizeal/javaapp06112024/operation/Square.java
@@ -2,10 +2,14 @@ package com.qualizeal.javaapp06112024.operation;
 
 public class Square extends GeometricShape {
 	public void area() {
-		System.out.println("Area of Square()");
+		int side = 10;
+		
+		System.out.println("Area of Square() = " + " " + side*side);
 	}
 
 	public void perimeter() {
-		System.out.println("Perimeter of Square");
+		int side = 10;
+		
+		System.out.println("Perimeter of Square = " + " " + 4*side);
 	}
 }
--- java-app-06112024/src/main/java/com/qualizeal/javaapp06112024/operation/Triangle.java
+++ java-app-06112024/src/main/java/com/qualizeal/javaapp06112024/operation/Triangle.java
@@ -2,10 +2,17 @@ package com.qualizeal.javaapp06112024.operation;
 
 public class Triangle extends GeometricShape {
 	public void area() {
-		System.out.println("Area of Triangle()");
+		int base = 10;
+		int height = 10;
+		
+		System.out.println("Area of Triangle() = " + " " + (base*height)/2);
 	}
 
 	public void perimeter() {
-		System.out.println("Perimeter of Triangle");
+		int a = 10;
+		int b = 10;
+		int c = 10;
+		
+		System.out.println("Perimeter of Triangle = " + " " + (a+b+c));
 	}
 }
