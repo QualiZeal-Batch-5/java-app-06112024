6650692637e2ecff31f4c723a9cade8c922b9272
mounikasriavidi <102526472+mounikasriavidi@users.noreply.github.com>
2024-11-06 14:54:35 +0530

    Animal.java

--- src/main/java/com/qualizeal/javaapp/Assignment1/Animal.java
+++ src/main/java/com/qualizeal/javaapp/Assignment1/Animal.java
@@ -2,6 +2,6 @@ package com.qualizeal.javaapp.Assignment1;
 
 public class Animal {
 	public void makeSound() {
-		System.out.println("Amba Ambaa");
+		System.out.println("makeSound() from Animal");
 	}
 }
